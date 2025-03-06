bc4b30e592cf9b9e99583ca62450bd3a5e539e06
karlo2002187 <karlogmb@gmail.com>
2025-03-06 00:42:49 -0800

    Renombrar server.js a index.js y configurar vercel.json

--- server.js
+++ server.js
@@ -11,48 +11,47 @@ const tokenAddress = process.env.TOKEN_ADDRESS;
 
 // ABI mínima para leer balances
 const minABI = [
-    {
-        "constant": true,
-        "inputs": [],
-        "name": "totalSupply",
-        "outputs": [{ "name": "", "type": "uint256" }],
-        "type": "function"
-    },
-    {
-        "constant": true,
-        "inputs": [{ "name": "owner", "type": "address" }],
-        "name": "balanceOf",
-        "outputs": [{ "name": "balance", "type": "uint256" }],
-        "type": "function"
-    }
+  {
+    "constant": true,
+    "inputs": [],
+    "name": "totalSupply",
+    "outputs": [{ "name": "", "type": "uint256" }],
+    "type": "function"
+  },
+  {
+    "constant": true,
+    "inputs": [{ "name": "owner", "type": "address" }],
+    "name": "balanceOf",
+    "outputs": [{ "name": "balance", "type": "uint256" }],
+    "type": "function"
+  }
 ];
 
 const tokenContract = new web3.eth.Contract(minABI, tokenAddress);
 
 // Endpoint para obtener el total supply
 app.get('/total-supply', async (req, res) => {
-    try {
-        const totalSupply = await tokenContract.methods.totalSupply().call();
-        res.send(web3.utils.fromWei(totalSupply, 'ether')); // Enviar solo el número
-    } catch (error) {
-        res.status(500).send(error.message);
-    }
+  try {
+    const totalSupply = await tokenContract.methods.totalSupply().call();
+    res.send(web3.utils.fromWei(totalSupply, 'ether')); // Enviar solo el número
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
 });
 
 // Endpoint para obtener el circulating supply
 app.get('/circulating-supply', async (req, res) => {
-    try {
-        const whaleBalance = await tokenContract.methods.balanceOf(process.env.WHALE_ADDRESS).call();
-        const totalSupply = await tokenContract.methods.totalSupply().call();
-        const circulatingSupply = totalSupply - whaleBalance;
-
-        res.send(web3.utils.fromWei(circulatingSupply.toString(), 'ether')); // Enviar solo el número
-    } catch (error) {
-        res.status(500).send(error.message);
-    }
+  try {
+    const whaleBalance = await tokenContract.methods.balanceOf(process.env.WHALE_ADDRESS).call();
+    const totalSupply = await tokenContract.methods.totalSupply().call();
+    const circulatingSupply = totalSupply - whaleBalance;
+    res.send(web3.utils.fromWei(circulatingSupply.toString(), 'ether')); // Enviar solo el número
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
 });
 
 // Iniciar servidor
 app.listen(PORT, () => {
-    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
